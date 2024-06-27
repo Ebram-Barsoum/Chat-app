4abb8650220f10a50cc0b3d6271d9041cefb50db
Ebram-Barsoum <abram.barsoum3.89@gmail.com>
2024-06-27 11:40:38 +0300

    'fix-logo-image-path-and-add-netlify-configuration-file'

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <img
-      src="/public/logo.png"
+      src="/logo.png"
       alt="ME-Chat logo image"
       className="w-[70%]  sm:h-[15rem] sm:w-[20rem] "
     />
